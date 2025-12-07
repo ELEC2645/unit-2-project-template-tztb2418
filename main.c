1a9a4322a58652c97fee006e7a8c8a735ef79ac8
tztb2418 <tztb2418@leeds.ac.uk>
2025-12-07 01:49:06 +0000

    JamesClothier_pproject_6/12/25

--- main.c
+++ main.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <math.h>
 
 #define PI 3.14159265358979323846
 
-//  ---  Function header  ---  //
+//  ---  Function headers  ---  //
+
+// Operational
 void clear_screen(void);
 void move_cursor(int x, int y);
 void draw_sin(int offset);
 void draw_crosshairs(void);
 void animate_sin(void);
 void get_amp_freq(void);
-
-char main_menu(void);
+// User input
+int user_input(char *sel_buf, size_t sel_buf_sz);
+void flush_input_buffer(void);
+// print/ disply
+void print_home_screen(void);
+void print_main_menu(void);
+void print_main_menu(void);
+void soz(void);
+// Program navigation
+void main_menu(void);
+void select_menu_item(char *sel_buf);
 
 //  ---  Global varibles  ---  //
 
@@ -26,17 +39,32 @@ float freq;
 
 const char mark = 'o';
 
+// string input buffers
+char sel_buf[20];
+char amp_buf[4];
+char freq_buf[5];
+char *endptr;
+
 int main() {
 
-    if(main_menu() == "Sine"){
-        get_amp_freq();
-        animate_sin(); 
+    
+    print_home_screen();
+    if(user_input(sel_buf, sizeof(sel_buf))){
+       exit; 
+    };
+    if(strcmp(sel_buf, "m") == 0){
+        main_menu(); 
+    }
+    else {
+        exit;
     }
   
     return 0;
 }
 
 //  ---   Functions def  ---   //
+
+// Operational - (sine)
 
 void clear_screen(void) {
     printf("\033[2J");    // clear output
@@ -118,11 +146,111 @@ void get_amp_freq(void){
     freq = (float)frequency;
 }
 
-char main_menu(void){
-    printf("  SIGNAL GENERATOR");
-    printf("\n    MAIN MENU:");
-    Printf("\n1. input Sine for sinusoidal wave form");
-    char str[];
-    scanf("%s", str);
-    return str;
+// User input
+
+int user_input(char *sel_buf, size_t sel_buf_sz) {
+    if (sel_buf == NULL || sel_buf_sz == 0) {
+        printf("Invalid Buffer");
+        return -1;
+    }
+    if (!fgets(sel_buf, (int)sel_buf_sz, stdin)) {
+        flush_input_buffer();
+        printf("fgets Fault");
+        return -1;
+    }
+
+    // If no newline found, input exceeded buffer; flush leftover
+    if (strchr(sel_buf, '\n') == NULL) {
+        flush_input_buffer();
+    }
+
+    // Strip newline
+    sel_buf[strcspn(sel_buf, "\n")] = '\0';
+
+    if (sel_buf[0] == '\0') {
+        // empty select word not allowed
+        return -1;
+    }
+
+    return 0;
+}
+
+void flush_input_buffer(void) {
+    int ch;
+    while ((ch = getchar()) != '\n' && ch != EOF) { /* discard */ }
+}
+
+void get_amp(void){
+    printf("\nPlease enter a amplitude between 1 and 100:");
+    user_input(amp_buf, sizeof(amp_buf));
+    amp = strtof(amp_buf, &endptr);
+}
+
+void get_freq(void){
+    printf("\nPlease enter a frequency between 1 and 8:");
+    user_input(freq_buf, sizeof(amp_buf));
+    freq = strtof(freq_buf, &endptr);
+}
+
+// print/ disply
+
+void print_home_screen(void){
+    printf("\n------------- Wave Form Generator -------------\n");
+    printf("Welcome, this is a waveform animation generator."); 
+    printf("\nPlease enter m to view the Wave Form Menu:");
+}
+
+void print_main_menu(void) {
+    printf("\n----------- Wave Form Menu -----------\n");
+    printf("|\tSine\t\t\t|\n");
+    printf("|\tSquare\t\t\t|\n");
+    printf("|\tSaw Tooth\t\t|\n");
+    printf("|\tInverse Saw\t\t|\n");
+    printf("|\tTriangle\t\t|\n");
+    printf("|\t\t\t\t|\n");
+    printf("|\tExit\t\t\t|\n");
+    printf("---------------------------------\n");
+    printf("\nPlease enter selection here:");
+}
+
+void soz(void){
+    printf("\nSorry, this waveform is yet to be developed.");
+    printf("\nPlease enter m for Wave Form Menu");
+}
+
+// Program navigation
+
+void main_menu() {
+    print_main_menu();
+    if(!user_input(sel_buf, sizeof(sel_buf))){
+        get_amp();
+        get_freq();
+        select_menu_item(sel_buf);
+    }
+    
+}
+
+void select_menu_item(char *sel_buf) {
+
+    if(strcmp(sel_buf, "sine") == 0) {
+        animate_sin();  
+    }
+    else if(strcmp(sel_buf, "square") == 0) {
+        soz();
+    }
+    else if(strcmp(sel_buf, "saw tooth") == 0) {
+        soz();
+    }
+    else if(strcmp(sel_buf, "inverse saw tooth") == 0) {
+        soz();
+    }
+    else if(strcmp(sel_buf, "triangle") == 0) {
+        soz();
+    }
+    else if(strcmp(sel_buf, "exit") == 0){
+        exit;
+    }
+    else {
+        printf("Invalid input: %s\n", sel_buf);
+    }
 }
